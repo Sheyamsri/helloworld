66f0011c0ef1b59f8b0c0c10544526c4cb12e204
Sheyamsri <94829694+Sheyamsri@users.noreply.github.com>
2022-02-10 08:38:25 -0500

    object created

---
+++ objectcreation.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main (String[] args) {
+    Main obj = new Main();
+    obj.Users();
+    Employee();
+    }
+    public void Users(){ 
+        System.out.println("User created");
+    }
+    public static void Employee(){
+        System.out.println("Employee created");
+    }
+}
